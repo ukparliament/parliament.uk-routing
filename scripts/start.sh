7bf63c9ed6f6c13fb2a5abb47f1154ae65e784b4
James McKay <code@jamesmckay.net>
2018-01-09 16:29:01 +0000

    Fix backend hints - always use directors

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,22 +2,30 @@
 
 function init_docker_compose() {
     cat <<EOF > /vcl/backends.vcl
-backend lists {
+import directors;
+
+backend lists_server {
     .host = "$LIST_BACKEND_IP";
     .port = "$LIST_BACKEND_PORT";
 }
 
-backend things {
+backend things_server {
     .host = "$THING_BACKEND_IP";
     .port = "$THING_BACKEND_PORT";
 }
 
-backend utilities {
+backend utilities_server {
     .host = "$UTILITIES_BACKEND_IP";
     .port = "$UTILITIES_BACKEND_PORT";
 }
 
 sub init_backends {
+    new lists = directors.round_robin();
+    lists.add_backend(lists_server);
+    new things = directors.round_robin();
+    things.add_backend(things_server);
+    new utilities = directors.round_robin();
+    utilities.add_backend(utilities_server);
 }
 EOF
     chmod +x /vcl/backends.vcl
