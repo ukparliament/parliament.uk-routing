ca4e9ddd441abd1287b69b23c21f06954d9d8d68
James McKay <code@jamesmckay.net>
2018-01-11 16:11:26 +0000

    Use Python scripts to create vcl for both Docker and AWS

--- scripts/build-vcl.py
+++ scripts/build-vcl.py
@@ -1,12 +1,10 @@
 #! /usr/bin/env python3
 
-import os
 import sys
 
-from vclbuilder.tasks import list_tasks
-from vclbuilder.vcl import VclFile
-
-def get_vcl():
+def get_vcl_aws():
+    from vclbuilder.tasks import list_tasks
+    from vclbuilder.vcl import VclFile
 
     tasks = list_tasks('ecs')
     vclfile = VclFile()
@@ -14,5 +12,25 @@ def get_vcl():
         vclfile.add_backend(task)
     return vclfile.get_vcl()
 
+
+def get_vcl_docker():
+    import os
+    import os.path
+    from string import Template
+
+    with open(os.path.join(os.path.dirname(__file__), 'compose.tpl')) as f:
+        txt = f.read()
+    tpl = Template(txt)
+    return tpl.substitute(os.environ)
+
+
+def get_vcl(is_aws):
+    if is_aws:
+        return get_vcl_aws()
+    else:
+        return get_vcl_docker()
+
+
 if __name__ == '__main__':
-    print(get_vcl())
+    is_aws = ('-a' in sys.argv) or ('--aws' in sys.argv)
+    print(get_vcl(is_aws))
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,54 +1,19 @@
-#!/bin/sh
+#! /bin/sh
 
-function init_docker_compose() {
-    cat <<EOF > /vcl/backends.vcl
-import directors;
-
-backend lists_server {
-    .host = "$LIST_BACKEND_IP";
-    .port = "$LIST_BACKEND_PORT";
-}
-
-backend things_server {
-    .host = "$THING_BACKEND_IP";
-    .port = "$THING_BACKEND_PORT";
-}
-
-backend utilities_server {
-    .host = "$UTILITIES_BACKEND_IP";
-    .port = "$UTILITIES_BACKEND_PORT";
-}
-
-sub init_backends {
-    new lists = directors.round_robin();
-    lists.add_backend(lists_server);
-    new things = directors.round_robin();
-    things.add_backend(things_server);
-    new utilities = directors.round_robin();
-    utilities.add_backend(utilities_server);
-}
-EOF
-    chmod +x /vcl/backends.vcl
-}
-
-
-function init_aws_ecr() {
+if [ -n "$LIST_BACKEND_IP" ]; then
+    # Running in docker compose
+    python3 /scripts/build-vcl.py > /vcl/backends.vcl
+else
+    # Running in AWS
+    init_aws_ecr
     mkdir -p ~/.aws
     cat <<EOF > ~/.aws/config
 [default]
 region=eu-west-1
 EOF
     chmod -R go-rwx ~/.aws
-    python3 /scripts/build-vcl.py > /vcl/backends.vcl
-}
-
-
-if [ -n "$LIST_BACKEND_IP" ]; then
-    init_docker_compose
-else
-    init_aws_ecr
+    python3 /scripts/build-vcl.py -a > /vcl/backends.vcl
 fi
-
 
 mkdir -p /var/lib/varnish/`hostname` && chown nobody /var/lib/varnish/`hostname`
 
