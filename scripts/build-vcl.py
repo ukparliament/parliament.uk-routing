eed65a564969db93c0a5177f0e4192b956377ede
James McKay <mckayj@parliament.uk>
2018-05-23 16:02:16 +0100

    Don't use ALBs for now.
    ALBs have multiple IP addresses mapped to their domain names and Varnish
    doesn't like this. We need to find out how to handle this before we can
    proceed.

--- scripts/build-vcl.py
+++ scripts/build-vcl.py
@@ -7,7 +7,7 @@ import sys
 import urllib.request
 
 # Set this to False if you want to use Varnish for load balancing
-USE_ALBS = True
+USE_ALBS = False
 
 def get_cluster_name():
     # Get the IP address of the container
